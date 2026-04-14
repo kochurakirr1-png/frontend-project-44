88e9c8929b2179a2bfeb9d49b0ddd229f5d6d0b5
Kirr <kochura.kirr1@gmail.com>
2026-04-15 00:06:52 +0300

    Add Brain GCD game

--- Makefile
+++ Makefile
@@ -13,3 +13,6 @@ brain-even:
 	node bin/brain-even.js
 brain-calc:
 	node bin/brain-calc.js
+brain-gcd:
+	node bin/brain-gcd.js
+
---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+// Функция нахождения НОД (алгоритм Евклида)
+const findGCD = (a, b) => {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
+
+const getGameData = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const question = num1 + ' ' + num2;
+  const correctAnswer = String(findGCD(num1, num2));
+
+  return { question, correctAnswer };
+};
+
+export default getGameData;
